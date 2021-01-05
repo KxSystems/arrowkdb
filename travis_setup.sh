93331ca74e1b6c9dd9aa3f3429326ae1cee6a46d
nmcdonnell-kx <nmcdonnell@kx.com>
2021-01-05 13:37:08 +0000

    Use xcode10.2

--- travis_setup.sh
+++ travis_setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-if [["$TRAVIS_OS_NAME" == "linux" ]]; then
-  sudo apt update
-  sudo apt install -y -V ca-certificates lsb-release wget
-  if [ $(lsb_release --codename --short) = "stretch" ]; then
-    sudo tee /etc/apt/sources.list.d/backports.list <<APT_LINE
+sudo apt update
+sudo apt install -y -V ca-certificates lsb-release wget
+if [ $(lsb_release --codename --short) = "stretch" ]; then
+  sudo tee /etc/apt/sources.list.d/backports.list <<APT_LINE
 deb http://deb.debian.org/debian $(lsb_release --codename --short)-backports main
 APT_LINE
-  fi
+fi
+  
+if [["$TRAVIS_OS_NAME" == "linux" ]]; then
   wget https://apache.bintray.com/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-archive-keyring-latest-$(lsb_release --codename --short).deb
   sudo apt install -y -V ./apache-arrow-archive-keyring-latest-$(lsb_release --codename --short).deb
   sudo apt update
