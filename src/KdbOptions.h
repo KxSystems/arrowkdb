6195db2321ea0358f66cb82747e1f72a8aa7d2db
Samuel Bruce <sbruce1@firstderivatives.com>
2022-09-20 11:47:18 +1000

    Updating Options for ORC Write

--- src/KdbOptions.h
+++ src/KdbOptions.h
@@ -21,6 +21,7 @@ namespace Options
   // Int options
   const std::string ARROW_CHUNK_ROWS = "ARROW_CHUNK_ROWS";
   const std::string PARQUET_CHUNK_SIZE = "PARQUET_CHUNK_SIZE";
+  const std::string ORC_CHUNK_SIZE = "ORC_CHUNK_SIZE";
   const std::string PARQUET_MULTITHREADED_READ = "PARQUET_MULTITHREADED_READ";
   const std::string USE_MMAP = "USE_MMAP";
   const std::string DECIMAL128_AS_DOUBLE = "DECIMAL128_AS_DOUBLE";
