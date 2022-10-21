08350f8efc8f42b00388f12e446c2d53c54383b2
Samuel Bruce <sbruce1@firstderivatives.com>
2022-10-21 16:43:49 +1100

    String Int Test

--- src/ArrayReader.cpp
+++ src/ArrayReader.cpp
@@ -172,10 +172,11 @@ void AppendArray(std::shared_ptr<arrow::Array> array_data, K k_array, size_t& in
     auto int32_type = std::static_pointer_cast<arrow::Int32Array>(int32_array->type());
     for (auto i = 0; i < int32_array->length(); ++i) {  
       // auto integer32 = arrow::int32(int32_array->Value(i)); // Doesn't work because it can't take arguments
-      auto integer32 = int32_array->Value(i);
-      // K k_int = ktn(KG, 16); // Can change the second argument to 16 or 32 or 64 to test
-      // integer32.ToBytes(kG(k_int));
-      kI(k_array)[index++] = integer32; // Might have to change kI? - Check back on Decimal way 
+      auto str_data = int32_array->Value(i);
+      str_data.ToString(kG(k_int));
+      K k_str = ktn(KC, str_data.length());
+      memcpy(kG(k_str), str_data.data(), str_data.length());
+      kK(k_array)[index++] = k_str;
     }
     // memcpy(kI(k_array), int32_array->raw_values(), int32_array->length() * sizeof(arrow::Int32Array::value_type));
     break;
