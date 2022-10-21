3b2fac8787890ead3455ec5caa2e3cbd47c9440e
Samuel Bruce <sbruce1@firstderivatives.com>
2022-10-21 11:19:19 +1100

    Adjusting Array Reader

--- src/ArrayReader.cpp
+++ src/ArrayReader.cpp
@@ -156,16 +156,16 @@ void AppendArray(std::shared_ptr<arrow::Array> array_data, K k_array, size_t& in
   }
   case arrow::Type::UINT32:
   {
-    auto uint32_array = std::static_pointer_cast<arrow::UInt32Array>(array_data);
-    memcpy(kI(k_array), uint32_array->raw_values(), uint32_array->length() * sizeof(arrow::UInt32Array::value_type));
+    // auto uint32_array = std::static_pointer_cast<arrow::UInt32Array>(array_data);
+    // memcpy(kI(k_array), uint32_array->raw_values(), uint32_array->length() * sizeof(arrow::UInt32Array::value_type));
+    auto uint64_array = std::static_pointer_cast<arrow::UInt64Array>(array_data);
+    memcpy(kJ(k_array), uint64_array->raw_values(), uint64_array->length() * sizeof(arrow::UInt64Array::value_type));
     break;
   }
   case arrow::Type::INT32:
   {
-    // auto int32_array = std::static_pointer_cast<arrow::Int32Array>(array_data);
-    // memcpy(kI(k_array), int32_array->raw_values(), int32_array->length() * sizeof(arrow::Int32Array::value_type));
-    auto int64_array = std::static_pointer_cast<arrow::Int64Array>(array_data);
-    memcpy(kJ(k_array), int64_array->raw_values(), int64_array->length() * sizeof(arrow::Int64Array::value_type));
+    auto int32_array = std::static_pointer_cast<arrow::Int32Array>(array_data);
+    memcpy(kI(k_array), int32_array->raw_values(), int32_array->length() * sizeof(arrow::Int32Array::value_type));
     break;
   }
   case arrow::Type::UINT64:
