116a19afbf0ba063372a1fc7adfa959559a25d27
Vyacheslav Grechin <vgrechin@kx.com>
2023-01-20 17:10:58 +0300

    Supporting of extended types writing

--- src/ArrayWriter.cpp
+++ src/ArrayWriter.cpp
@@ -31,7 +31,7 @@ bool is_equal( T lhs, T rhs )
 {
     static const T epsilon = 2 * std::numeric_limits<T>::epsilon();
 
-    return ::fabs(lhs -= rhs) <= epsilon;
+    return ::fabs( lhs -= rhs ) <= epsilon;
 }
 
 shared_ptr<arrow::ArrayBuilder> GetBuilder(shared_ptr<arrow::DataType> datatype);
@@ -370,7 +370,7 @@ void PopulateListBuilder(shared_ptr<arrow::DataType> datatype, K k_array, arrow:
       continue;
 
     // Delimit the start/end of each child list set
-    list_builder->Append();
+    PARQUET_THROW_NOT_OK( list_builder->Append() );
 
     if (datatype->id() == arrow::Type::FIXED_SIZE_LIST) {
       // Check each sub-list is the same length as the fixed size
@@ -415,7 +415,7 @@ void PopulateUnionBuilder(shared_ptr<arrow::DataType> datatype, K k_array, arrow
   // for this union value
   for (auto index = 0; index < kK(k_array)[0]->n; ++index) {
     int8_t live_type_id = kH(type_ids)[index];
-    union_builder->Append(live_type_id);
+    PARQUET_THROW_NOT_OK( union_builder->Append(live_type_id) );
   }
 
   // Populate each of the child builders from its kdb list, starting from 1 to
@@ -563,9 +563,9 @@ void PopulateBuilder<arrow::Type::UINT64>(shared_ptr<arrow::DataType> datatype, 
   if( type_overrides.null_mapping.have_uint64 ){
     std::vector<bool> null_bitmap( k_array->n );
     for( auto i = 0ll; i < k_array->n; ++i ){
-      null_bitmap[i] = type_overrides.null_mapping.uint64_null ^ kI( k_array )[i];
+      null_bitmap[i] = type_overrides.null_mapping.uint64_null ^ kJ( k_array )[i];
     }
-    PARQUET_THROW_NOT_OK( uint64_builder->AppendValues( ( uint64_t* )kI( k_array ), k_array->n, null_bitmap ) );
+    PARQUET_THROW_NOT_OK( uint64_builder->AppendValues( ( uint64_t* )kJ( k_array ), k_array->n, null_bitmap ) );
   }
   else{
     PARQUET_THROW_NOT_OK(uint64_builder->AppendValues((uint64_t*)kJ(k_array), k_array->n));
@@ -579,9 +579,9 @@ void PopulateBuilder<arrow::Type::INT64>(shared_ptr<arrow::DataType> datatype, K
   if( type_overrides.null_mapping.have_int64 ){
     std::vector<bool> null_bitmap( k_array->n );
     for( auto i = 0ll; i < k_array->n; ++i ){
-      null_bitmap[i] = type_overrides.null_mapping.int64_null ^ kI( k_array )[i];
+      null_bitmap[i] = type_overrides.null_mapping.int64_null ^ kJ( k_array )[i];
     }
-    PARQUET_THROW_NOT_OK( int64_builder->AppendValues( ( int64_t* )kI( k_array ), k_array->n, null_bitmap ) );
+    PARQUET_THROW_NOT_OK( int64_builder->AppendValues( ( int64_t* )kJ( k_array ), k_array->n, null_bitmap ) );
   }
   else{
     PARQUET_THROW_NOT_OK(int64_builder->AppendValues((int64_t*)kJ(k_array), k_array->n));
@@ -658,7 +658,7 @@ void PopulateBuilder<arrow::Type::STRING>(shared_ptr<arrow::DataType> datatype, 
       K str_data = kK( k_array )[i];
       TYPE_CHECK_ITEM( str_data->t != KC, datatype->ToString(), KC, str_data->t );
       if( type_overrides.null_mapping.have_string
-          && type_overrides.null_mapping.string_null.length() == str_data->n
+          && type_overrides.null_mapping.string_null.length() == static_cast<std::size_t>( str_data->n )
           && !type_overrides.null_mapping.string_null.compare( 0, str_data->n, ( char* )kG( str_data ), str_data->n ) ){
         PARQUET_THROW_NOT_OK( str_builder->AppendNull() );
       }
@@ -691,7 +691,7 @@ void PopulateBuilder<arrow::Type::LARGE_STRING>(shared_ptr<arrow::DataType> data
       K str_data = kK( k_array )[i];
       TYPE_CHECK_ITEM( str_data->t != KC, datatype->ToString(), KC, str_data->t );
       if( type_overrides.null_mapping.have_large_string
-          && type_overrides.null_mapping.large_string_null.length() == str_data->n
+          && type_overrides.null_mapping.large_string_null.length() == static_cast<std::size_t>( str_data->n )
           && !type_overrides.null_mapping.large_string_null.compare( 0, str_data->n, ( char* )kG( str_data ), str_data->n ) ){
         PARQUET_THROW_NOT_OK( str_builder->AppendNull() );
       }
@@ -710,7 +710,7 @@ void PopulateBuilder<arrow::Type::BINARY>(shared_ptr<arrow::DataType> datatype, 
     K bin_data = kK(k_array)[i];
     TYPE_CHECK_ITEM(bin_data->t != KG, datatype->ToString(), KG, bin_data->t);
     if( type_overrides.null_mapping.have_binary
-        && type_overrides.null_mapping.binary_null.length() == bin_data->n
+        && type_overrides.null_mapping.binary_null.length() == static_cast<std::size_t>( bin_data->n )
         && !type_overrides.null_mapping.binary_null.compare( 0, bin_data->n, kG( bin_data ), bin_data->n ) ){
       PARQUET_THROW_NOT_OK( bin_builder->AppendNull() );
     }
@@ -728,7 +728,7 @@ void PopulateBuilder<arrow::Type::LARGE_BINARY>(shared_ptr<arrow::DataType> data
     K bin_data = kK(k_array)[i];
     TYPE_CHECK_ITEM(bin_data->t != KG, datatype->ToString(), KG, bin_data->t);
     if( type_overrides.null_mapping.have_large_binary
-        && type_overrides.null_mapping.large_binary_null.length() == bin_data->n
+        && type_overrides.null_mapping.large_binary_null.length() == static_cast<std::size_t>( bin_data->n )
         && !type_overrides.null_mapping.large_binary_null.compare( 0, bin_data->n, kG( bin_data ), bin_data->n ) ){
       PARQUET_THROW_NOT_OK( bin_builder->AppendNull() );
     }
@@ -751,7 +751,14 @@ void PopulateBuilder<arrow::Type::FIXED_SIZE_BINARY>(shared_ptr<arrow::DataType>
       K bin_data = kK(k_array)[i];
       TYPE_CHECK_ITEM(bin_data->t != KG, datatype->ToString(), KG, bin_data->t);
       TYPE_CHECK_LENGTH(fixed_bin_builder->byte_width() != bin_data->n, builder->type()->ToString(), fixed_bin_builder->byte_width(), bin_data->n);
-      PARQUET_THROW_NOT_OK(fixed_bin_builder->Append(kG(bin_data)));
+      if( type_overrides.null_mapping.have_fixed_binary
+          && type_overrides.null_mapping.fixed_binary_null.length() == static_cast<std::size_t>( bin_data->n )
+          && !type_overrides.null_mapping.fixed_binary_null.compare( 0, bin_data->n, kG( bin_data ), bin_data->n ) ){
+        PARQUET_THROW_NOT_OK( fixed_bin_builder->AppendNull() );
+      }
+      else{
+        PARQUET_THROW_NOT_OK(fixed_bin_builder->Append(kG(bin_data)));
+      }
     }
   }
 }
@@ -794,7 +801,13 @@ void PopulateBuilder<arrow::Type::TIMESTAMP>(shared_ptr<arrow::DataType> datatyp
   auto ts_builder = static_cast<arrow::TimestampBuilder*>(builder);
   auto timestamp_type = static_pointer_cast<arrow::TimestampType>(datatype);
   for (auto i = 0; i < k_array->n; ++i)
-    PARQUET_THROW_NOT_OK(ts_builder->Append(tc.KdbToArrow(kJ(k_array)[i])));
+    if( type_overrides.null_mapping.have_timestamp
+        && type_overrides.null_mapping.timestamp_null == kJ( k_array )[i] ){
+      PARQUET_THROW_NOT_OK( ts_builder->AppendNull() );
+    }
+    else{
+      PARQUET_THROW_NOT_OK(ts_builder->Append(tc.KdbToArrow(kJ(k_array)[i])));
+    }
 }
 
 template<>
@@ -804,7 +817,13 @@ void PopulateBuilder<arrow::Type::TIME32>(shared_ptr<arrow::DataType> datatype, 
   auto t32_builder = static_cast<arrow::Time32Builder*>(builder);
   auto time32_type = static_pointer_cast<arrow::Time32Type>(datatype);
   for (auto i = 0; i < k_array->n; ++i)
-    PARQUET_THROW_NOT_OK(t32_builder->Append(tc.KdbToArrow(kI(k_array)[i])));
+    if( type_overrides.null_mapping.have_time32
+        && type_overrides.null_mapping.time32_null == kI( k_array )[i] ){
+      PARQUET_THROW_NOT_OK( t32_builder->AppendNull() );
+    }
+    else{
+      PARQUET_THROW_NOT_OK(t32_builder->Append(tc.KdbToArrow(kI(k_array)[i])));
+    }
 }
 
 template<>
@@ -814,7 +833,13 @@ void PopulateBuilder<arrow::Type::TIME64>(shared_ptr<arrow::DataType> datatype, 
   auto t64_builder = static_cast<arrow::Time64Builder*>(builder);
   auto time64_type = static_pointer_cast<arrow::Time64Type>(datatype);
   for (auto i = 0; i < k_array->n; ++i)
-    PARQUET_THROW_NOT_OK(t64_builder->Append(tc.KdbToArrow(kJ(k_array)[i])));
+    if( type_overrides.null_mapping.have_time64
+        && type_overrides.null_mapping.time64_null == kJ( k_array )[i] ){
+      PARQUET_THROW_NOT_OK( t64_builder->AppendNull() );
+    }
+    else{
+      PARQUET_THROW_NOT_OK(t64_builder->Append(tc.KdbToArrow(kJ(k_array)[i])));
+    }
 }
 
 template<>
@@ -824,10 +849,16 @@ void PopulateBuilder<arrow::Type::DECIMAL>(shared_ptr<arrow::DataType> datatype,
   auto dec_type = static_pointer_cast<arrow::Decimal128Type>(datatype);
   for (auto i = 0; i < k_array->n; ++i) {
     if (type_overrides.decimal128_as_double) {
-      // Construct the decimal from a double
-      arrow::Decimal128 dec128;
-      PARQUET_ASSIGN_OR_THROW(dec128, arrow::Decimal128::FromReal(kF(k_array)[i], dec_type->precision(), dec_type->scale()));
-      PARQUET_THROW_NOT_OK(dec_builder->Append(dec128));
+      if( type_overrides.null_mapping.have_decimal
+          && is_equal( type_overrides.null_mapping.decimal_null, kF( k_array )[i] ) ){
+        PARQUET_THROW_NOT_OK( dec_builder->AppendNull() );
+      }
+      else{
+        // Construct the decimal from a double
+        arrow::Decimal128 dec128;
+        PARQUET_ASSIGN_OR_THROW(dec128, arrow::Decimal128::FromReal(kF(k_array)[i], dec_type->precision(), dec_type->scale()));
+        PARQUET_THROW_NOT_OK(dec_builder->Append(dec128));
+      }
     } else {
       // Each decimal is a list of 16 bytes
       K k_dec = kK(k_array)[i];
@@ -847,7 +878,13 @@ void PopulateBuilder<arrow::Type::DURATION>(shared_ptr<arrow::DataType> datatype
   auto dur_builder = static_cast<arrow::DurationBuilder*>(builder);
   auto duration_type = static_pointer_cast<arrow::DurationType>(datatype);
   for (auto i = 0; i < k_array->n; ++i)
-    PARQUET_THROW_NOT_OK(dur_builder->Append(tc.KdbToArrow(kJ(k_array)[i])));
+    if( type_overrides.null_mapping.have_duration
+        && type_overrides.null_mapping.duration_null == kJ( k_array )[i] ){
+      PARQUET_THROW_NOT_OK( dur_builder->AppendNull() );
+    }
+    else{
+      PARQUET_THROW_NOT_OK(dur_builder->Append(tc.KdbToArrow(kJ(k_array)[i])));
+    }
 }
 
 template<>
@@ -917,7 +954,7 @@ void PopulateBuilder<arrow::Type::MAP>(shared_ptr<arrow::DataType> datatype, K k
       continue;
 
     // Delimit the start/end of each child map set
-    map_builder->Append();
+    PARQUET_THROW_NOT_OK( map_builder->Append() );
 
     // Populate the child builders for this map set from the dictionary key/value lists
     auto k_dict = kK(k_array)[i];
@@ -948,7 +985,7 @@ void PopulateBuilder<arrow::Type::STRUCT>(shared_ptr<arrow::DataType> datatype, 
 
   // Delimit each struct value in the parent builder
   for (auto index = 0; index < kK(k_array)[0]->n; ++index)
-    struct_builder->Append();
+    PARQUET_THROW_NOT_OK( struct_builder->Append() );
 
   // Populate each of the field builders from its kdb list.  Only count up to
   // the number of struct fields.  Additional trailing data in the kdb mixed
@@ -1113,7 +1150,7 @@ K prettyPrintArray(K datatype_id, K array, K options)
   auto arrow_array = MakeArray(datatype, array, type_overrides);
   auto options = arrow::PrettyPrintOptions();
   string result;
-  arrow::PrettyPrint(*arrow_array, options, &result);
+  PARQUET_THROW_NOT_OK( arrow::PrettyPrint(*arrow_array, options, &result) );
 
   return kp((S)result.c_str());
 
